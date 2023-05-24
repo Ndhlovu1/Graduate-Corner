9d1feed95966f47f950c945d83655116f3a59f10
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-24 23:58:19 +0200

    Variable Declarations

--- app/src/main/java/com/example/graduate_corner/users/LoginActivity.java
+++ app/src/main/java/com/example/graduate_corner/users/LoginActivity.java
@@ -4,15 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.graduate_corner.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button login;
+    TextView forgot_pwd;
+    TextView signup;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.txt_signup);
+
+        auth = FirebaseAuth.getInstance();
+
+
     }
 }
