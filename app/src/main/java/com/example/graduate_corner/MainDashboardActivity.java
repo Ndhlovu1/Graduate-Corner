605dbbe29d390e4b6dc97f116e57c8094d64e075
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 01:20:23 +0200

    Error Codes Updates

--- app/src/main/java/com/example/graduate_corner/MainDashboardActivity.java
+++ app/src/main/java/com/example/graduate_corner/MainDashboardActivity.java
@@ -1,12 +1,18 @@
 package com.example.graduate_corner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.GridLayout;
 
+import com.example.graduate_corner.mentors.MentorsActivity;
+import com.example.graduate_corner.notes.NotesActivity;
+import com.example.graduate_corner.skills_building.SkillsActivity;
+import com.example.graduate_corner.videos.VideosActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainDashboardActivity extends AppCompatActivity {
@@ -31,7 +37,33 @@ public class MainDashboardActivity extends AppCompatActivity {
 
     }
 
+    //Set the Direction for the MainGrid Cards being Viewed
+    private void setSingleEvent(GridLayout mainGrid) {
 
+        for (int i = 0; i<mainGrid.getChildCount(); i++){
+
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(v -> {
+                if (finalI == 0){
+                    startActivity(new Intent(MainDashboardActivity.this, NotesActivity.class));
+                }
+
+                else if (finalI == 1){
+                    startActivity(new Intent(MainDashboardActivity.this, SkillsActivity.class));
+                }
+                else if (finalI == 2){
+                    startActivity(new Intent(MainDashboardActivity.this, VideosActivity.class));
+                }
+                else if (finalI == 3){
+                    startActivity(new Intent(MainDashboardActivity.this, MentorsActivity.class));
+                }
+
+            });
+
+        }
+
+    }
 
 
 
