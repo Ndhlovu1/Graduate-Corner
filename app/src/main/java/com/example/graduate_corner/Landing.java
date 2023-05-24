1f0dea108aa95fa2bc2f9489fc82be9a3fb49008
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-24 22:51:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/graduate_corner/Landing.java
@@ -0,0 +1,19 @@
+package com.example.graduate_corner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+    }
+
+
+
+
+}
