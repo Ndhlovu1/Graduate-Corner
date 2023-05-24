24d5133083f20d6e949475a851db181d9ce0adc4
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 01:16:13 +0200

    Project Code Update

---
+++ app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduate_corner.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.graduate_corner.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
---
+++ app/src/main/java/com/example/graduate_corner/skills_building/SkillsActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduate_corner.skills_building;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.graduate_corner.R;
+
+public class SkillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skills);
+    }
+}
