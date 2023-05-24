064ab68bbfff2b8078494029e897cfb862c664d0
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-24 23:47:17 +0200

    Completed full screen view for the app

--- app/src/main/java/com/example/graduate_corner/MainDashboardActivity.java
+++ app/src/main/java/com/example/graduate_corner/MainDashboardActivity.java
@@ -3,12 +3,14 @@ package com.example.graduate_corner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainDashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_dashboard);
     }
 }
--- app/src/main/java/com/example/graduate_corner/users/ProfileActivity.java
+++ app/src/main/java/com/example/graduate_corner/users/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.graduate_corner.users;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.graduate_corner.R;
 
@@ -11,6 +12,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_profile);
     }
 }
