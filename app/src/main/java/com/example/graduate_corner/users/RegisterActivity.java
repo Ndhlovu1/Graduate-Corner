2e66908c019d0a16f471b6ebe5a64cf38722f17c
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-24 23:54:20 +0200

    Updated Layout Designs

--- app/src/main/java/com/example/graduate_corner/users/RegisterActivity.java
+++ app/src/main/java/com/example/graduate_corner/users/RegisterActivity.java
@@ -14,6 +14,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
-        //Merge
+        //
     }
 }
