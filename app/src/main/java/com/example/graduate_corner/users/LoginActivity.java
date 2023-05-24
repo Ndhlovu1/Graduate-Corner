46b301559a0a5f280b0998b76de4caf1c5e8d313
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 00:00:37 +0200

    Variable Declarations

--- app/src/main/java/com/example/graduate_corner/users/LoginActivity.java
+++ app/src/main/java/com/example/graduate_corner/users/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.graduate_corner.users;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +15,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Declare variable types that are/have been used in the views
     EditText email, password;
     Button login;
     TextView forgot_pwd;
     TextView signup;
+    //Create a Firebase Authentication Instance
     FirebaseAuth auth;
+
 
 
     @Override
@@ -26,12 +31,23 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+        //Match the variables to their respective views
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
         signup = findViewById(R.id.txt_signup);
 
+        //Get the authenticated instance of the database, assigned by default
         auth = FirebaseAuth.getInstance();
+
+        //Direct the User to the Sign Up activity
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
 
 
     }
