0df5693ecbc40574bba37f0ca23b99d3d481db09
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-24 23:29:50 +0200

    Assigned the redirect views from theLanding to the Login or the Register

--- app/src/main/java/com/example/graduate_corner/Landing.java
+++ app/src/main/java/com/example/graduate_corner/Landing.java
@@ -2,14 +2,64 @@ package com.example.graduate_corner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.graduate_corner.users.LoginActivity;
+import com.example.graduate_corner.users.RegisterActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Landing extends AppCompatActivity {
+
+    Button lgn_btn, register;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //Should the user be logged in already, then simply forward to logged in screen
+        if (firebaseUser != null){
+            startActivity(new Intent(Landing.this, MainDashboardActivity.class));
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing);
+
+        //Assign the views to their respective variable names
+        lgn_btn = findViewById(R.id.login_btn);
+        register = findViewById(R.id.register_btn);
+
+        //Launch an explicit event into the Login Activity
+        lgn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Landing.this, LoginActivity.class));
+                finish();
+
+            }
+        });
+
+        //Launch an explicit event into the Register Activity
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Landing.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+
 
     }
 
---
+++ app/src/main/java/com/example/graduate_corner/MainDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduate_corner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+    }
+}
