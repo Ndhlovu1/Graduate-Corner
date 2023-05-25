15fdb5c0c4f798abf3df44ac499f486ba1f007d9
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 22:01:49 +0200

    Writing to Room has been implemented

--- app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
+++ app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
@@ -1,20 +1,34 @@
 package com.example.graduate_corner.notes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.example.graduate_corner.MainDashboardActivity;
 import com.example.graduate_corner.R;
+import com.example.graduate_corner.notes.Models.Notes;
+import com.example.graduate_corner.notes.Models.RoomDb;
+import com.example.graduate_corner.notes.adapters.NotesListAdapter;
 import com.example.graduate_corner.users.LoginActivity;
 import com.example.graduate_corner.users.ProfileActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -22,15 +36,85 @@ public class NotesActivity extends AppCompatActivity {
     FirebaseAuth auth;
     DrawerLayout drawerLayout;
 
+    RecyclerView recyclerView;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    RoomDb database;
+    FloatingActionButton fab_add;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_notes);
 
         drawerLayout = findViewById(R.id.drawerlayoutNotes);
         auth = FirebaseAuth.getInstance();
 
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+        database = RoomDb.getInstance(this);
+        notes = database.notesDAO().getAll();
+
+        updateRecycler(notes);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(NotesActivity.this, NotesAddActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+
     }
+
+    //Accept the Data passed
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101){
+            if (resultCode == Activity.RESULT_OK){
+                //Save the data to the db
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                //Add it to the db
+                database.notesDAO().insert(new_notes);
+                //Update arraylist
+                notes.clear();
+                //Recall all items
+                notes.addAll(database.notesDAO().getAll());
+                //Update adapter on i
+                notesListAdapter.notifyDataSetChanged();
+
+
+            }
+        }
+
+    }
+
+    private void updateRecycler(List<Notes> notes) {
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        notesListAdapter=new NotesListAdapter(NotesActivity.this, notes, notesClickListener );
+        recyclerView.setAdapter(notesListAdapter);
+
+    }
+
+    private final NotesClickListener notesClickListener = new NotesClickListener() {
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
+    };
+
 
     //Verify if the user hasnt clicked logout to kill this activity
     @Override
