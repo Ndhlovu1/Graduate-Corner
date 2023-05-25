040d33537df9b435c1bf2515bc7f1bfa08612148
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 13:31:31 +0200

    Notes Dashboard Layout Implemented

--- app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
+++ app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
@@ -1,12 +1,18 @@
 package com.example.graduate_corner.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 import com.example.graduate_corner.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NotesActivity extends AppCompatActivity {
+
+    //Create the specific variables for Firebase
+    FirebaseAuth auth;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
