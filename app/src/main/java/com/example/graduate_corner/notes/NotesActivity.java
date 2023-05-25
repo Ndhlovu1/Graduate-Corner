ef50c34ff6cc2b55bde75ee977096637e167acc3
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-25 13:37:17 +0200

    NavBar - Notes Updated

--- app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
+++ app/src/main/java/com/example/graduate_corner/notes/NotesActivity.java
@@ -1,12 +1,20 @@
 package com.example.graduate_corner.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.graduate_corner.MainDashboardActivity;
 import com.example.graduate_corner.R;
+import com.example.graduate_corner.users.LoginActivity;
+import com.example.graduate_corner.users.ProfileActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -19,7 +27,84 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-
+        drawerLayout = findViewById(R.id.drawerlayoutNotes);
+        auth = FirebaseAuth.getInstance();
 
     }
+
+    //Verify if the user hasnt clicked logout to kill this activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+        if (firebaseUser != null){
+            //String userName = firebaseUser.getEmail();
+            //Toast.makeText(this, "Welcome Back!", Toast.LENGTH_SHORT).show();
+        }else {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
+    }
+
+    //Drawer Code Starts Here
+    public void clickMenu(View view){
+        //open Drawer
+        openDrawer(drawerLayout);
+    }
+
+    private static void  openDrawer(DrawerLayout drawerLayout) {
+
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    private void closeDrawer(DrawerLayout drawerLayout) {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }//End of Drawer Code
+
+    public void clickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public void clickDashboard(View view){
+        //Recreate Activity
+        // recreate();
+        redirectActivity(this, MainDashboardActivity.class);
+    }
+
+    public void clickProfile(View view){
+        redirectActivity(this, ProfileActivity.class);
+    }
+
+    public void logout(View view){
+        //Log the user out of their Account
+        //Get Out of the App
+        // System.exit(0);
+        auth.signOut();
+        finish();
+    }
+
+    //Taking the User to the Start Activity
+    private static void redirectActivity(Activity activity, Class aClass) {
+
+        //initialize intent
+        Intent intent = new Intent(activity,aClass);
+        //setFlag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Start Activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        closeDrawer(drawerLayout);
+        super.onPause();
+    }
+
+
+
+
 }
